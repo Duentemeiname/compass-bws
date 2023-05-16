b93640891f6484da39d668b250d6930e614d508f
Duentemeiname <119460193+Duentemeiname@users.noreply.github.com>
2023-05-16 08:50:26 +0200

    Add files via upload

---
+++ function/download.php
@@ -0,0 +1,83 @@
+<?php
+session_start();
+require_once ('../function/DBconfig.php');
+
+
+$ID_PHP = $_GET["id"];
+$user = $_GET["user"];
+
+//Prüfung ob der Nutzer uinternes Login hat (wird bei status.php mitgegeben)
+if($user == "extern")
+{
+    if($ID_PHP != $_SESSION["ID_PW"]) //Siehe status.php
+    {
+       echo "Fehler bei der Zuordnung des Antrages.";
+       exit();
+    }
+    $verifiziert = "true";
+}
+ if((empty($ID_PHP) || empty($_SESSION ["user_logged_in"])) && empty($_SESSION["passwort"])) //Prüfung ob die richtigen Informationen übergeben wurden
+ {
+    echo "Bei Ihrer Anfrage ist ein Fehler aufgetreten.";
+    exit();
+ }
+
+ else
+ {
+    //Anfrage an die Datenbank - JOIN zwischen Antrage und Dateiuploads um berechtigung prüfen zu können.
+    $Anfrage = "SELECT antraege_beurlaubung.ID_PHP, antraege_beurlaubung.kuerzel_tutor, antraege_beurlaubung_dateiupload.Dateiname 
+                FROM antraege_beurlaubung 
+                INNER JOIN antraege_beurlaubung_dateiupload ON antraege_beurlaubung.ID_PHP = antraege_beurlaubung_dateiupload.ID_PHP 
+                WHERE antraege_beurlaubung.ID_PHP = $ID_PHP
+                ";
+    $ergebnis = $db_link->query($Anfrage); //SQL Abfrage wird an die Datenbank übergeben 
+
+    //Antwort der DB auf Korrektheit prüfen
+    if(!$ergebnis)
+        {
+            echo "Ungültige ID! ";
+            exit();
+        }
+    if($ergebnis->num_rows == 1)
+        {
+            $daten = $ergebnis->fetch_array(); 
+            $ID_PHP_DB = $daten[0];
+            $kuerzel_tutor = $daten[1];
+            $Dateiname = $daten[2];
+
+            if($ID_PHP_DB != $ID_PHP) //Prüfung ob die ID die die DB liefert der angefragte entspricht 
+            {
+                echo "Die Datenbank hat ein falsches Ergebnis geliefert.";
+                exit();
+            }
+
+            if(empty($verifiziert)) //Prüfung ob von  status.php verfiziert siehe oben in dieser Datei
+            {
+            if($_SESSION["SL"] != "true") //Prüfung ob SL dann fällt Prüfung auf tutor zugriff weg
+            {
+                if($kuerzel_tutor != $_SESSION["Kuerzel"]) //Prüfung ob der Tutor zugreifen darf
+                {
+                    echo "Sie haben keine Berechtigung diese Datei herunterzuladen.";
+                    exit();
+                }
+            }
+            }
+            $filepath = '../uploads/'.$Dateiname;
+
+            if (!file_exists($filepath) || !is_readable($filepath)) 
+            {
+                echo "Dateizugriff gescheitert. ";
+                exit();
+            }
+            header('Content-Type: application/pdf');
+            header('Content-Disposition: inline; filename="' . basename($filepath) . '"');
+            readfile($filepath);
+            exit();
+        }
+    else
+        {
+            echo "Es ist ein Fehler beim Datenbankabruf aufgetreten (Datei nicht eindeutig). ";
+            exit();   
+        }
+ }
+?>
